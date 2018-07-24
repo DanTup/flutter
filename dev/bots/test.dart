a093dd6460bf76be69aa7f654a61d4a2fcbc5c96
Danny Tuppeny <dantup@google.com>
2018-07-24 09:43:43 +0100

    Fix path

--- dev/bots/test.dart
+++ dev/bots/test.dart
@@ -260,11 +260,11 @@ Future<Null> _runSmokeTests() async {
 Future<Null> _runToolTests() async {
   //await _runSmokeTests();
 
-  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools', 'test', 'integration'));
-  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools', 'test', 'integration'));
-  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools', 'test', 'integration'));
-  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools', 'test', 'integration'));
-  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools', 'test', 'integration'));
+  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools'), 'test/integration'));
+  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools'), 'test/integration'));
+  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools'), 'test/integration'));
+  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools'), 'test/integration'));
+  await _pubRunTest(path.join(flutterRoot, 'packages', 'flutter_tools'), 'test/integration'));
 
   print('${bold}DONE: All tests successful.$reset');
 }
