91adf08d950a80e2294095a8e171ddce15c9e0ad
Danny Tuppeny <dantup@google.com>
2018-07-24 11:31:12 +0100

    Look up breakpoints rather than relying on magic numbers

--- packages/flutter_tools/test/integration/test_data/basic_project.dart
+++ packages/flutter_tools/test/integration/test_data/basic_project.dart
@@ -29,7 +29,7 @@ class BasicProject extends TestProject {
     @override
     Widget build(BuildContext context) {
       topLevelFunction();
-      return new MaterialApp(
+      return new MaterialApp( // BREAKPOINT
         title: 'Flutter Demo',
         home: new Container(),
       );
@@ -37,18 +37,13 @@ class BasicProject extends TestProject {
   }
 
   topLevelFunction() {
-    print("topLevelFunction");
+    print("topLevelFunction"); // 2 BREAKPOINT
   }
   ''';
 
-  @override
-  String get breakpointFile => buildMethodBreakpointFile;
-  @override
-  int get breakpointLine => buildMethodBreakpointLine;
-
-  String get buildMethodBreakpointFile => fs.path.join(dir.path, 'lib', 'main.dart');
-  int get buildMethodBreakpointLine => 9;
+  String get buildMethodBreakpointFile => breakpointFile;
+  int get buildMethodBreakpointLine => breakpointLine;
 
   String get topLevelFunctionBreakpointFile => fs.path.join(dir.path, 'lib', 'main.dart');
-  int get topLevelFunctionBreakpointLine => 17;
+  int get topLevelFunctionBreakpointLine => lineContaining(main, '// 2 BREAKPOINT');
 }
