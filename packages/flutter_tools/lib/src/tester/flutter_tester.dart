14a70f37a4c6a6642c9357d2de34dc505be682c5
Danny Tuppeny <dantup@google.com>
2018-09-03 10:18:32 +0100

    Dump all output from flutter_Tester

--- packages/flutter_tools/lib/src/tester/flutter_tester.dart
+++ packages/flutter_tools/lib/src/tester/flutter_tester.dart
@@ -117,6 +117,8 @@ class FlutterTesterDevice extends Device {
       '--non-interactive',
       '--enable-dart-profiling',
       '--packages=${PackageMap.globalPackagesPath}',
+      '--verbose-logging',
+      '--trace-startup',
     ];
     if (debuggingOptions.debuggingEnabled) {
       if (debuggingOptions.startPaused)
@@ -158,12 +160,14 @@ class FlutterTesterDevice extends Device {
           .transform(utf8.decoder)
           .transform(const LineSplitter())
           .listen((String line) {
+        printTrace(line);
         _logReader.addLine(line);
       });
       _process.stderr
           .transform(utf8.decoder)
           .transform(const LineSplitter())
           .listen((String line) {
+        printTrace(line);
         _logReader.addLine(line);
       });
 
