dbd7b28f2c501b083de595af097fccd3ab379496
Danny Tuppeny <danny@tuppeny.com>
2026-09-09 09:51:16 +0100

    Add extra logging to widget preview

--- packages/flutter_tools/lib/src/widget_preview/dtd_services.dart
+++ packages/flutter_tools/lib/src/widget_preview/dtd_services.dart
@@ -169,7 +169,9 @@ class WidgetPreviewDtdServices {
   }
 
   Future<FlutterWidgetPreviews> getFlutterWidgetPreviews() async {
+    logger.printWarning('Running _waitForLspService from getFlutterWidgetPreviews');
     await _waitForLspService();
+    logger.printWarning('Done running _waitForLspService from getFlutterWidgetPreviews');
     const maxAttempts = 50;
     for (var attempts = 0; attempts < maxAttempts; attempts++) {
       try {
@@ -191,7 +193,13 @@ class WidgetPreviewDtdServices {
   }
 
   Future<FlutterWidgetPreviews> getFlutterWidgetPreviewsForFile({required String filePath}) async {
+    logger.printWarning(
+      'Running _waitForLspService from getFlutterWidgetPreviewsForFile ($filePath)',
+    );
     await _waitForLspService();
+    logger.printWarning(
+      'Done running _waitForLspService from getFlutterWidgetPreviewsForFile ($filePath)',
+    );
     const maxAttempts = 50;
     for (var attempts = 0; attempts < maxAttempts; attempts++) {
       try {
@@ -202,6 +210,9 @@ class WidgetPreviewDtdServices {
         );
         return FlutterWidgetPreviews.fromJson(result.result['result']! as Map<String, Object?>);
       } on RpcException catch (e) {
+        logger.printWarning(
+          'Exception running _waitForLspService from getFlutterWidgetPreviewsForFile ($filePath)',
+        );
         if (e.code == -32601 && attempts < maxAttempts - 1) {
           // Method not found
           await Future<void>.delayed(const Duration(milliseconds: 200));
